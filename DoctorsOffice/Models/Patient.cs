7db7411d86ab31c7e6687c3dcb0bd9a52ab8df8a
Bai Jaitrong <bjaitrong@comcast.net>
2023-01-08 14:57:58 -0700

    Add Doctor and Patient class along with DB tables.
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>

---
+++ DoctorsOffice/Models/Patient.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+
+namespace DoctorsOffice.Models
+{
+  public class Patient
+  {
+    public int PatientId { get; set;}
+    public string Name { get; set;}
+
+    public int DoctorId { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public List<DoctorPatient> JoinEntities { get; }
+  }
+}
